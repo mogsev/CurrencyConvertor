db9f2c9ac684466de19dfcfc61b42c74f64a6734
zhenya <mogsev@gmail.com>
2015-07-28 23:44:19 +0300

    update on home

--- app/src/main/java/com/mogsev/currencyconvertor/MainActivity.java
+++ app/src/main/java/com/mogsev/currencyconvertor/MainActivity.java
@@ -2,8 +2,6 @@ package com.mogsev.currencyconvertor;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,59 +11,43 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
-
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-
 public class MainActivity extends AppCompatActivity {
-    public TextView value;
-
-    private Handler handler;
+    private final static String urlWebService = "http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?";
+    private final static String conversionRateResult = "ConversionRateResult";
+    private final static String conversionRateException = "ConversionRateException";
+    private TextView value;
+    private Spinner spinnerFromCurrency;
+    private Spinner spinnerToCurrency;
+    private ArrayAdapter<CharSequence> adapterCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinnerFromCurrency = (Spinner) this.findViewById(R.id.from_currency);
-        Spinner spinnerToCurrency = (Spinner) this.findViewById(R.id.to_currency);
+        spinnerFromCurrency = (Spinner) this.findViewById(R.id.from_currency);
+        spinnerToCurrency = (Spinner) this.findViewById(R.id.to_currency);
 
-        ArrayAdapter<CharSequence> adapterFromCurrency = ArrayAdapter.createFromResource(this,
+         adapterCurrency = ArrayAdapter.createFromResource(this,
                 R.array.currency, android.R.layout.simple_spinner_item );
-        adapterFromCurrency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterCurrency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinnerFromCurrency.setAdapter(adapterFromCurrency);
-        spinnerToCurrency.setAdapter(adapterFromCurrency);
+        spinnerFromCurrency.setAdapter(adapterCurrency);
+        spinnerToCurrency.setAdapter(adapterCurrency);
 
-        final TextView value = (TextView) this.findViewById(R.id.value);
+        value = (TextView) this.findViewById(R.id.value);
         value.setText("1234567890");
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                if (msg.what == 0) {
-                    value.setText("00000");
-                }
-            }
-        };
     }
 
     @Override
@@ -91,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getRateCash(View view) {
-
             Log.d("getRate", "Begin getRate");
             Thread thread = new Thread(new Runnable() {
                 @Override
@@ -126,65 +107,68 @@ public class MainActivity extends AppCompatActivity {
                     } catch (Exception ex) {
                         Log.d("getRate", ex.toString());
                     }
-                        handler.sendEmptyMessage(0);
+                        //handler.sendEmptyMessage(0);
                 }
             });
         thread.start();
         Log.d("getRate", "End getRate");
     }
 
-    public void getRate(View view) {
+    /**
+     * Example result - "http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=USD&ToCurrency=UAH"
+     * @return
+     */
+    private String getUrl() {
+        //"http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=USD&ToCurrency=UAH"
+        StringBuilder url = new StringBuilder();
+        url.append(urlWebService);
+        // get position spinners
+        int numFromCurrency = spinnerFromCurrency.getSelectedItemPosition();
+        int numToCurrency = spinnerToCurrency.getSelectedItemPosition();
+        // get string result spinners
+        String from = adapterCurrency.getItem(numFromCurrency).toString();
+        String to = adapterCurrency.getItem(numToCurrency).toString();
 
-        ConversionRate conversionRate = new ConversionRate(); // Создаем экземпляр
-        conversionRate.execute(); // запускаем
-        /**
-        Log.d("getRate", "Begin getRate");
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL urlConversion = new URL("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=USD&ToCurrency=UAH");
-                    URLConnection connection = urlConversion.openConnection();
-                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder db = dbf.newDocumentBuilder();
-                    Document doc = db.parse(connection.getInputStream());
-                    Log.d("getRate", doc.getDocumentElement().getTextContent());
-                } catch (Exception ex) {
-                    Log.d("getRate", ex.toString());
-                }
-                handler.sendEmptyMessage(0);
-            }
-        });
-        thread.start();
-        Log.d("getRate", "End getRate");
-
-         */
+        url.append("FromCurrency=").append(from);
+        url.append("&ToCurrency=").append(to);
+        Log.d("getUrl", url.toString());
+        return url.toString();
     }
 
-    class ConversionRate extends AsyncTask<Void, Void, String> {
+    /**
+     *
+     * @param view
+     */
+    public void getRate(View view) {
+        ConversionRate conversionRate = new ConversionRate(); // Создаем экземпляр
+        conversionRate.execute(getUrl()); // запускаем
+    }
+
+    /**
+     *
+     */
+    private class ConversionRate extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(String... params) {
             String result = null;
             try {
-                URL urlConversion = new URL("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=USD&ToCurrency=UAH");
+                URL urlConversion = new URL(params[0]);
                 URLConnection connection = urlConversion.openConnection();
                 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
                 DocumentBuilder db = dbf.newDocumentBuilder();
                 Document doc = db.parse(connection.getInputStream());
                 result = doc.getDocumentElement().getTextContent();
-                Log.d("getRate", result);
+                Log.d(conversionRateResult, result);
             } catch (Exception ex) {
-                Log.d("getRate", ex.toString());
+                Log.d(conversionRateException, ex.toString());
             }
             return result;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            //super.onPostExecute(s);
+            super.onPostExecute(s);
             value.setText(String.valueOf(s));
         }
     }
-
-
 }
