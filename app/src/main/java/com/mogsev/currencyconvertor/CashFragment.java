7921bd6d436c43411b24cea0cb5557c46ebea277
zhenya <mogsev@gmail.com>
2015-08-17 11:10:43 +0300

    add progress bar for Cash Fragment

--- app/src/main/java/com/mogsev/currencyconvertor/CashFragment.java
+++ app/src/main/java/com/mogsev/currencyconvertor/CashFragment.java
@@ -8,15 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.LinearLayout;
 
 import com.mogsev.util.CashLoader;
 import com.mogsev.util.CashAdapter;
 import com.mogsev.util.CurrencyInformer;
-import com.mogsev.util.FinanceAdapter;
-import com.mogsev.util.FinanceLoader;
 
 import java.util.List;
 
@@ -26,13 +22,10 @@ import java.util.List;
 public class CashFragment extends ListFragment implements LoaderManager.LoaderCallbacks<List<CurrencyInformer>> {
     private static final String TAG = "CashFragment";
     private static final int LOADER_CASH = 1;
-    private static final int LOADER_FINANCE = 2;
     CashAdapter cashAdapter;
+    private LinearLayout linearProgress;
 
     private View view;
-    //private String[] cityData;
-    //private ArrayAdapter<String> spinnerAdapter;
-    //private Spinner spinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,34 +36,15 @@ public class CashFragment extends ListFragment implements LoaderManager.LoaderCa
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // Initialize spinner start
-        /**
-        cityData = getResources().getStringArray(R.array.city);
-        spinner = (Spinner) view.findViewById(R.id.spinnerCityInformer);
-        spinnerAdapter = new ArrayAdapter(getActivity().getBaseContext(), android.R.layout.simple_spinner_item, cityData);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Initialize progress bar loader
+        linearProgress = (LinearLayout) view.findViewById(R.id.loaderProgress);
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        // Initialize spinner end
-         */
-
+        // Initialize CashAdapter
         cashAdapter = new CashAdapter(getActivity());
         setListAdapter(cashAdapter);
 
-
         // Initialize LoadManager
         getLoaderManager().initLoader(LOADER_CASH, null, this);
-        //getLoaderManager().initLoader(LOADER_FINANCE, null, this);
     }
 
     @Override
@@ -82,14 +56,9 @@ public class CashFragment extends ListFragment implements LoaderManager.LoaderCa
     @Override
     public Loader<List<CurrencyInformer>> onCreateLoader(int id, Bundle args) {
         Log.d(TAG, "onCreateLoader start");
-        Loader loader = null;
-        switch (id) {
-            case LOADER_CASH:
-                Log.d(TAG, "Create LOADER_CASH");
-                loader = new CashLoader(getActivity());
-                break;
-        }
-        return loader;
+        // Show progress bar
+        linearProgress.setVisibility(View.VISIBLE);
+        return new CashLoader(getActivity());
     }
 
     @Override
@@ -97,13 +66,13 @@ public class CashFragment extends ListFragment implements LoaderManager.LoaderCa
         Log.d(TAG, "onLoadFinished start");
         // Set the new data in the adapter.
         cashAdapter.setData(data);
+        // Hide progress bar
+        linearProgress.setVisibility(View.GONE);
     }
-
-
 
     @Override
     public void onLoaderReset(Loader<List<CurrencyInformer>> loader) {
         Log.d(TAG, "onLoaderReset start");
-        //cashAdapter.setData(null);
+        cashAdapter.setData(null);
     }
 }
